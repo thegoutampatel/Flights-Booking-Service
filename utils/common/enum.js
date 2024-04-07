ae652461511c80392c3ba3386f21872a687125d4
thegoutampatel <goutampatel9131@gmail.com>
2024-04-07 17:45:42 +0530

    Added Booking Model along with Utils from Flights service

---
+++ utils/common/enum.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+    BUSINESS : 'business',
+    ECONOMY: 'economy',
+    PREMIUM_ECONOMY: 'premium-economy',
+    FIRST_CLASS: 'first-class'
+}
+
+const BOOKING_STATUS = {
+    BOOKED : 'booked',
+    CANCELLED: 'cancelled',
+    INITIATED: 'initiated',
+    PANDING: 'panding'
+}
+
+module.exports = {
+    SEAT_TYPE,
+    BOOKING_STATUS
+}
